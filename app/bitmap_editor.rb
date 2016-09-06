7ec191a77604b1631f5ecbb92ffd5934584054d4
Ben <ben.c.hawker@gmail.com>
2016-09-06 22:24:40 +0800

    Create help class, remove from app/bitmap_editor class

--- app/bitmap_editor.rb
+++ app/bitmap_editor.rb
@@ -1,4 +1,5 @@
 require_relative '../lib/bitmap'
+require_relative '../lib/help'
 
 class BitmapEditor
 
@@ -20,19 +21,13 @@ class BitmapEditor
   end
 
   private
-    def exit_console
-      puts 'goodbye!'
-      @running = false
-    end
 
-    def show_help
-      puts "? - Help
-I M N - Create a new M x N image with all pixels coloured white (O).
-C - Clears the table, setting all pixels to white (O).
-L X Y C - Colours the pixel (X,Y) with colour C.
-V X Y1 Y2 C - Draw a vertical segment of colour C in column X between rows Y1 and Y2 (inclusive).
-H X1 X2 Y C - Draw a horizontal segment of colour C in row Y between columns X1 and X2 (inclusive).
-S - Show the contents of the current image
-X - Terminate the session"
-    end
+  def exit_console
+    puts 'Goodbye!'
+    @running = false
+  end
+
+  def show_help
+    Help.show
+  end
 end
