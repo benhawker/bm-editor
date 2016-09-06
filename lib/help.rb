853da6c91e0d90de6120d26b7beebbf77aa821dc
Ben <ben.c.hawker@gmail.com>
2016-09-06 22:45:43 +0800

    Adds class Help - completes removal from app

---
+++ lib/help.rb
@@ -0,0 +1,14 @@
+class Help
+
+  def self.show
+    puts "? - Help
+I M N - Create a new M x N image with all pixels coloured white (O).
+C - Clears the table, setting all pixels to white (O).
+L X Y C - Colours the pixel (X,Y) with colour C.
+V X Y1 Y2 C - Draw a vertical segment of colour C in column X between rows Y1 and Y2 (inclusive).
+H X1 X2 Y C - Draw a horizontal segment of colour C in row Y between columns X1 and X2 (inclusive).
+S - Show the contents of the current image
+X - Terminate the session"
+  end
+
+end
