6ff7fe43491c4d69d11153fc9aadce5a3b28aab2
Ben <ben.c.hawker@gmail.com>
2016-09-07 00:17:50 +0800

    Add executor to the app/bitmap_editor

--- app/bitmap_editor.rb
+++ app/bitmap_editor.rb
@@ -1,26 +1,32 @@
 require_relative '../lib/bitmap'
+require_relative '../lib/executor'
 require_relative '../lib/help'
 
 class BitmapEditor
 
   def run
     @running = true
-    puts 'type ? for help'
+    puts 'Type ? for Help'
     while @running
       print '> '
       input = gets.chomp
+
       case input
-        when '?'
-          show_help
-        when 'X'
-          exit_console
-        else
-          puts 'unrecognised command :('
+      when '?'
+        show_help
+      when 'X'
+        exit_console
+      else
+        executor.execute(input)
       end
     end
   end
 
   private
+
+  def executor
+    Executor.new
+  end
 
   def exit_console
     puts 'Goodbye!'
