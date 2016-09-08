7d14f940ff59ae30b2a457d9cd021133e07dbc31
Ben <ben.c.hawker@gmail.com>
2016-09-09 00:32:48 +0800

    Refactors errors into separate file

---
+++ lib/errors.rb
@@ -0,0 +1,43 @@
+# Errors related to Executor class
+class InvalidCommandCalled < StandardError
+  def initialize(command)
+    super("We don't recognise that command - you called #{command}. Try using ? to pull up the Help prompt")
+  end
+end
+
+class DowncasedCommandCalled < StandardError
+  def initialize(command)
+    super("The command letter is valid but we only accept capital letters. Call #{command.upcase}.")
+  end
+end
+
+class CreateABitmapFirst < StandardError
+  def initialize
+    super("You don't seem to have created a Bitmap. Create one using I.")
+  end
+end
+
+# Errors related to Bitmap class
+class InvalidBitmapSize < StandardError
+  def initialize
+    super("The specified size is invalid. X and Y must both fall between #{Bitmap::MIN_SIZE} & #{Bitmap::MAX_SIZE} inclusively.")
+  end
+end
+
+class OutOfBoundsError < StandardError
+  def initialize(x, y, width, height)
+    super("[#{x}, #{y}] is out of bounds. It must be within width 1 to #{width} and height 1 to #{height}")
+  end
+end
+
+class SegmentOutOfBoundsError < StandardError
+  def initialize
+    super("One or more of the pixels in this segment are out of bounds")
+  end
+end
+
+class InvalidColorError < StandardError
+  def initialize(color)
+    super("#{color} is not valid - must be a capital letter A-Z")
+  end
+end
